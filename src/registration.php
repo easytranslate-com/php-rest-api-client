c2de5cfa509b860ddb8b5cb042382d84b6cf9439
Petar <petar@customgento.com>
2021-08-03 12:16:06 +0200

    Remove registration file

--- src/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::LIBRARY, 'EasyTranslate_RestApiClient', __DIR__);
